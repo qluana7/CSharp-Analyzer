8055e2e0f69fc95ad3605d1fc4337c700ad3b041
Lukince <53417057+Lukince@users.noreply.github.com>
2021-05-27 15:42:55 +0900

    Change Preview Project Name

--- CSAnalyzer-Preview/Properties/Resources.Designer.cs
+++ CSAnalyzer-Preview/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace CSAnalyzer.Properties {
+namespace CSAnalyzer_Preview.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace CSAnalyzer.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CSAnalyzer.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("CSAnalyzer_Preview.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
