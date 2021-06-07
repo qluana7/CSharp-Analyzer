40993805583d31c237536167ce26745539d570aa
Lukince <53417057+Lukince@users.noreply.github.com>
2021-06-07 13:22:22 +0900

    Add Icon

--- CSAnalyzer-Preview/Properties/Resources.Designer.cs
+++ CSAnalyzer-Preview/Properties/Resources.Designer.cs
@@ -209,5 +209,15 @@ namespace CSAnalyzer_Preview.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   (아이콘)과(와) 유사한 System.Drawing.Icon 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
+        internal static System.Drawing.Icon WindowIcon {
+            get {
+                object obj = ResourceManager.GetObject("WindowIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
